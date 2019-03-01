778bcba76f989e6b9442f5fb0805ca09362b63d3
Furqan Ahmad <furqanahmad21@hotmail.com>
2019-03-01 15:35:44 +0500

    Admin request mapping configured for index method

--- src/main/java/com/spring/security/thymeleaf/controller/AdminController.java
+++ src/main/java/com/spring/security/thymeleaf/controller/AdminController.java
@@ -4,19 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("admin")
 public class AdminController {
 
-	@GetMapping("/index")
+	@GetMapping(value = {"/", "/index"})
 	public String index(ModelMap model) {
 		model.addAttribute("view", "admin/index");
-//	ModelAndView view = new ModelAndView();
-//	view.setViewName("fragments/main");
-//	view.addObject("view", "admin/index");
 		return "fragments/main";
 	}
 }
