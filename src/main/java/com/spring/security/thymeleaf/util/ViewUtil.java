1626d73424b802c81307d9f83b36158ceb71dcbc
Furqan <furqanahmad21@hotmail.com>
2019-03-03 01:59:39 +0500

    Added comments for ViewUtil class

--- src/main/java/com/spring/security/thymeleaf/util/ViewUtil.java
+++ src/main/java/com/spring/security/thymeleaf/util/ViewUtil.java
@@ -2,10 +2,23 @@ package com.spring.security.thymeleaf.util;
 
 import org.springframework.ui.ModelMap;
 
+/**
+ * @author Furqan
+ */
 public class ViewUtil {
 	
+	/**
+	 * Set the master page address
+	 */
 	private static final String MAIN_VIEW = "fragments/main";
 	
+	
+	/**
+	 * @param model Should be of type ModelMap.
+	 * @param pageTitle Title you want to display on browser tab.
+	 * @param view Location of the view e.g view/user/index
+	 * @return main view location.
+	 */
 	public static String generateView(ModelMap model, String pageTitle, String view) {
 		model.addAttribute("pageTitle", pageTitle);
 		model.addAttribute("view", view);
