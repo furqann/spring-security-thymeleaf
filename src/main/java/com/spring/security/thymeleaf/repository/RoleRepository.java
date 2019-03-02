a5e2b3042e6f3ebe183f1abcd5a764bfe01fb7b3
Furqan <furqanahmad21@hotmail.com>
2019-03-02 23:28:25 +0500

    Extended role repository by JpaRepository

--- src/main/java/com/spring/security/thymeleaf/repository/RoleRepository.java
+++ src/main/java/com/spring/security/thymeleaf/repository/RoleRepository.java
@@ -1,5 +1,16 @@
 package com.spring.security.thymeleaf.repository;
 
-public class RoleRepository {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spring.security.thymeleaf.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
 
 }
