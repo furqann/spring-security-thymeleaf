72cf471f933319cb07a9dd203fc7c544e66a6823
Furqan <furqanahmad21@hotmail.com>
2019-03-07 22:58:36 +0500

    Created RoleController and wrote JPQL for user list in desc order

---
+++ src/main/java/com/spring/security/thymeleaf/controller/RoleController.java
@@ -0,0 +1,14 @@
+package com.spring.security.thymeleaf.controller;
+
+public class RoleController {
+
+	//GET Index
+	
+	//GET Add
+	//POST Add
+	
+	//GET Edit
+	//POST Edit
+	
+	//GET Delete
+}
--- src/main/java/com/spring/security/thymeleaf/repository/UserRepository.java
+++ src/main/java/com/spring/security/thymeleaf/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.spring.security.thymeleaf.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,6 +13,9 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	
 	@Query("SELECT u FROM User u where u.userId = :id")
 	User findById(int id);
+	
+	@Query("SELECT u from User u order by u.userId desc")
+	List<User> findAll();
 	
 	@Query("SELECT u FROM User u where u.email = :emailAddress")
 	User findByEmail(String emailAddress);
