c284487400d3b715b7bb65e444a7a2c69370c9f4
Furqan <furqanahmad21@hotmail.com>
2019-03-07 23:35:08 +0500

    Added pages for roles and some mendings

--- src/main/java/com/spring/security/thymeleaf/controller/RoleController.java
+++ src/main/java/com/spring/security/thymeleaf/controller/RoleController.java
@@ -1,10 +1,30 @@
 package com.spring.security.thymeleaf.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.security.thymeleaf.service.RoleService;
+import com.spring.security.thymeleaf.util.ViewUtil;
+
+@Controller
+@RequestMapping("role")
 public class RoleController {
 
+	@Autowired
+	RoleService roleService;
+	
 	//GET Index
+	@GetMapping(value = {"/","/index"})
+	public String index(ModelMap model) {
+		model.addAttribute("roles", roleService.findAll());
+		return ViewUtil.generateView(model, "Roles", "role/index");
+	}
 	
 	//GET Add
+	
 	//POST Add
 	
 	//GET Edit
