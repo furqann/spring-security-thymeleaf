1810a74fc68a175f1c33b2f294c820255771283c
Furqan <furqanahmad21@hotmail.com>
2019-03-10 22:57:06 +0500

    BookController added and index method implemented

---
+++ src/main/java/com/spring/security/thymeleaf/controller/BookController.java
@@ -0,0 +1,23 @@
+package com.spring.security.thymeleaf.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.security.thymeleaf.service.BookService;
+import com.spring.security.thymeleaf.util.ViewUtil;
+
+@Controller
+@RequestMapping("book")
+public class BookController {
+	
+	@Autowired
+	BookService bookService;
+	
+	@GetMapping(value = {"/","/index","**"})
+	public String index(ModelMap model) {
+		return ViewUtil.generateView(model, "Book Rack", "book/index");
+	}
+}
