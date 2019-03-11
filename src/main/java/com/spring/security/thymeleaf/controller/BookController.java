03e34a3a1e582ee0adc92dd2b24828adcf272385
Furqan <furqanahmad21@hotmail.com>
2019-03-11 22:34:53 +0500

    Bug fix for BookController for saving book Get to Post

--- src/main/java/com/spring/security/thymeleaf/controller/BookController.java
+++ src/main/java/com/spring/security/thymeleaf/controller/BookController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.spring.security.thymeleaf.model.Book;
@@ -23,7 +24,7 @@ public class BookController {
 		return ViewUtil.generateView(model, "Book Rack", "book/index");
 	}
 	
-	@GetMapping("/add")
+	@PostMapping("/add")
 	public String save(ModelMap model, Book bookForm) {
 		bookService.save(bookForm);
 		return "redirect:index";
