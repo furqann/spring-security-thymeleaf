1667447a3276e43f871bd07bdfacb9efc00402cd
Furqan <furqanahmad21@hotmail.com>
2019-03-21 23:42:42 +0500

    Created new login page and its stylesheet

--- src/main/java/com/spring/security/thymeleaf/controller/LoginController.java
+++ src/main/java/com/spring/security/thymeleaf/controller/LoginController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("login")
 public class LoginController {
+	
 	@GetMapping("/")
 	public String login() {
-		return "testing";
+		return "login/login";
 	}
 }
