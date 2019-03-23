ec9c0d86353d7355ea4ae240c1d054bc3a3307a2
Furqan <furqanahmad21@hotmail.com>
2019-03-23 22:26:25 +0500

    Added user in-memory for test purpose

--- src/main/java/com/spring/security/thymeleaf/config/WebSecurityConfig.java
+++ src/main/java/com/spring/security/thymeleaf/config/WebSecurityConfig.java
@@ -1,9 +1,12 @@
 package com.spring.security.thymeleaf.config;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 //@Configuration
 @EnableWebSecurity
@@ -34,6 +37,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/css/**","/js/**");
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth.inMemoryAuthentication().withUser("furqan").password(encoder.encode("123")).roles("ADMIN");
 	}
 	
 }
